411637dc09ec227a0665aa4306834720aa972e72
stevenmarp <100309799+stevenmarp@users.noreply.github.com>
2026-07-02 12:55:00 +0700

    sm_custom_error_dialog 19.0: configurable error dialog titles

--- sm_custom_error_dialog/__manifest__.py
+++ sm_custom_error_dialog/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Custom Error Dialog",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Productivity",
     "summary": "Rename Odoo error dialog titles (Validation, Access, User, and all errors) from Settings",
     "description": """
@@ -32,6 +32,7 @@ Features
         ],
     },
     "images": [
+        "static/description/banner.gif",
         "static/description/icon.png",
     ],
     "installable": True,
